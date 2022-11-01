f5126da996cba0404ad62121b5cc3c857821b36f
Dylan Hu <git@dylan.hu>
2022-11-01 10:45:57 -0400

    Initial Commit

---
+++ Heath/HeathApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeathApp.swift
+//  Heath
+//
+//  Created by Dylan Hu on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct HeathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
