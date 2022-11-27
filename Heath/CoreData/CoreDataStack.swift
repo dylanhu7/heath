150f1648da77feaeecbbe47d25cc54c9ff865e7b
Dylan Hu <git@dylan.hu>
2022-11-26 21:14:20 -0500

    fix CoreDataStack

--- Heath/CoreData/CoreDataStack.swift
+++ Heath/CoreData/CoreDataStack.swift
@@ -7,12 +7,31 @@
 
 import CoreData
 import CloudKit
+import Combine
 
 /**
  Class which provides an interface to the CloudKit-backed Core Data stack
  */
 final class CoreDataStack: ObservableObject {
     static let shared: CoreDataStack = CoreDataStack()
+    
+    static var preview: CoreDataStack = {
+        let result = CoreDataStack(inMemory: true)
+        let viewContext = result.persistentContainer.viewContext
+        for _ in 0..<10 {
+            let newItem = Ledger(context: viewContext)
+            newItem.createdAt = Date.now
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
     
     var context: NSManagedObjectContext {
         persistentContainer.viewContext
@@ -38,6 +57,63 @@ final class CoreDataStack: ObservableObject {
     lazy var persistentContainer: NSPersistentCloudKitContainer = initializePersistentCloudKitContainer()
     
     lazy var cloudKitContainer: CKContainer = CKContainer(identifier: Config.containerIdentifier)
+    
+    /**
+     An operation queue for handling history processing tasks: watching changes, deduplicating tags, and triggering UI updates if needed.
+     */
+    private lazy var historyQueue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+    
+    /**
+     Track the last history token processed for a store, and write its value to file.
+     
+     The historyQueue reads the token when executing operations and updates it after processing is complete.
+     */
+    private var lastHistoryToken: NSPersistentHistoryToken? = nil {
+        didSet {
+            guard let token = lastHistoryToken,
+                  let data = try? NSKeyedArchiver.archivedData( withRootObject: token, requiringSecureCoding: true) else { return }
+            
+            do {
+                try data.write(to: tokenFile)
+            } catch {
+                print("###\(#function): Failed to write token data. Error = \(error)")
+            }
+        }
+    }
+    
+    /**
+     The file URL for persisting the persistent history token.
+     */
+    private lazy var tokenFile: URL = {
+        let url = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("CoreDataCloudKitDemo", isDirectory: true)
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print("###\(#function): Failed to create persistent container URL. Error = \(error)")
+            }
+        }
+        return url.appendingPathComponent("token.data", isDirectory: false)
+    }()
+    
+    private var subscriptions: Set<AnyCancellable> = []
+    private let inMemory: Bool
+    
+    init(inMemory: Bool = false) {
+        self.inMemory = inMemory
+        // Load the last token from the token file.
+        if let tokenData = try? Data(contentsOf: tokenFile) {
+            do {
+                lastHistoryToken = try NSKeyedUnarchiver.unarchivedObject(ofClass: NSPersistentHistoryToken.self, from: tokenData)
+            } catch {
+                print("###\(#function): Failed to unarchive NSPersistentHistoryToken. Error = \(error)")
+            }
+        }
+    }
 }
 
 // MARK: - Modify Core Data
@@ -93,7 +169,6 @@ extension CoreDataStack {
             print("Unable to get CKShare")
             return nil
         }
-        share[CKShare.SystemFieldKey.title] = ledger.title
         return share
     }
     
@@ -122,29 +197,22 @@ extension CoreDataStack {
 // MARK: - Initialize Container
 extension CoreDataStack {
     private func initializePersistentCloudKitContainer() -> NSPersistentCloudKitContainer {
-        let (privateStoreFolderURL, sharedStoreFolderURL) = createPersistentStoresDirectory()
         let container = NSPersistentCloudKitContainer(name: "Heath")
-        
-        /// Associate default store with the CloudKit private database
         guard let privateStoreDescription = container.persistentStoreDescriptions.first else {
             fatalError("#\(#function): Failed to retrieve a persistent store description.")
         }
-        privateStoreDescription.url = privateStoreFolderURL.appendingPathComponent("private.sqlite")
-        /// Enable history tracking
+        let storesURL = inMemory ? privateStoreDescription.url!.deletingLastPathComponent() : URL(fileURLWithPath: "/dev/null")
+        privateStoreDescription.url = storesURL.appendingPathComponent("private.sqlite")
         privateStoreDescription.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
-        /// Enable remote notifications
         privateStoreDescription.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
-        /// Specify database scope
-        let cloudKitContainerOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: Config.containerIdentifier)
-        cloudKitContainerOptions.databaseScope = .private
-        privateStoreDescription.cloudKitContainerOptions = cloudKitContainerOptions
         
         /// Add a second store and associate it with the CloudKit shared database
         guard let sharedStoreDescription = privateStoreDescription.copy() as? NSPersistentStoreDescription else {
             fatalError("#\(#function): Copying the private store description returned an unexpected value.")
         }
-        sharedStoreDescription.url = sharedStoreFolderURL.appendingPathComponent("shared.sqlite")
-        let sharedStoreOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: Config.containerIdentifier)
+        sharedStoreDescription.url = storesURL.appendingPathComponent("shared.sqlite")
+        let containerIdentifier = privateStoreDescription.cloudKitContainerOptions!.containerIdentifier
+        let sharedStoreOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: containerIdentifier)
         sharedStoreOptions.databaseScope = .shared
         sharedStoreDescription.cloudKitContainerOptions = sharedStoreOptions
         
@@ -162,6 +230,7 @@ extension CoreDataStack {
         
         /// Automatically merge the changes from other contexts.
         container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.transactionAuthor = appTransactionAuthorName
         container.viewContext.automaticallyMergesChangesFromParent = true
         
         /// Pin the viewContext to the current generation token and set it to keep itself up-to-date with local changes.
@@ -171,41 +240,67 @@ extension CoreDataStack {
             fatalError("Failed to pin viewContext to the current generation: \(error)")
         }
         
-        /**
-         Run initializeCloudKitSchema() once to update the CloudKit schema every time you change the Core Data model.
-         Don't call this code in the production environment.
-         */
-#if InitializeCloudKitSchema
-        do {
-            try container.initializeCloudKitSchema()
-        } catch {
-            print("\(#function): initializeCloudKitSchema: \(error)")
-        }
-#endif
+        // Observe Core Data remote change notifications.
+        NotificationCenter.default
+            .publisher(for: .NSPersistentStoreRemoteChange)
+            .sink {
+                self.processRemoteStoreChange($0)
+            }
+            .store(in: &subscriptions)
+        
         
         return container
     }
-    
+}
+
+// MARK: - Notifications
+extension CoreDataStack {
     /**
-     Create `CoreDataStores` directory to hold private and shared stores
-     
-     Private store at `CoreDataStores/Private`
-     Shared store at `CoreDataStores/Shared`
+     Handle remote store change notifications (.NSPersistentStoreRemoteChange).
      */
-    private func createPersistentStoresDirectory() -> (privateStoreFolderURL: URL, sharedStoreFolderURL: URL) {
-        let baseURL = NSPersistentContainer.defaultDirectoryURL()
-        let storeFolderURL = baseURL.appendingPathComponent("CoreDataStores")
-        let privateStoreFolderURL = storeFolderURL.appendingPathComponent("Private")
-        let sharedStoreFolderURL = storeFolderURL.appendingPathComponent("Shared")
-        
-        let fileManager = FileManager.default
-        for folderURL in [privateStoreFolderURL, sharedStoreFolderURL] where !fileManager.fileExists(atPath: folderURL.path) {
-            do {
-                try fileManager.createDirectory(at: folderURL, withIntermediateDirectories: true, attributes: nil)
-            } catch {
-                fatalError("#\(#function): Failed to create the store folder: \(error)")
+    @objc
+    func processRemoteStoreChange(_ notification: Notification) {
+        // Process persistent history to merge changes from other coordinators.
+        historyQueue.addOperation {
+            self.processPersistentHistory()
+        }
+    }
+}
+
+// MARK: - Persistent history processing
+extension CoreDataStack {
+    /**
+     Process persistent history, posting any relevant transactions to the current view.
+     */
+    func processPersistentHistory() {
+        let backgroundContext = persistentContainer.newBackgroundContext()
+        backgroundContext.performAndWait {
+            
+            // Fetch history received from outside the app since the last token
+            let historyFetchRequest = NSPersistentHistoryTransaction.fetchRequest!
+            historyFetchRequest.predicate = NSPredicate(format: "author != %@", appTransactionAuthorName)
+            let request = NSPersistentHistoryChangeRequest.fetchHistory(after: lastHistoryToken)
+            request.fetchRequest = historyFetchRequest
+            
+            let result = (try? backgroundContext.execute(request)) as? NSPersistentHistoryResult
+            guard let transactions = result?.result as? [NSPersistentHistoryTransaction],
+                  !transactions.isEmpty
+            else { return }
+            
+            print("transactions = \(transactions)")
+            self.mergeChanges(from: transactions)
+            
+            // Update the history token using the last transaction.
+            lastHistoryToken = transactions.last!.token
+        }
+    }
+    
+    private func mergeChanges(from transactions: [NSPersistentHistoryTransaction]) {
+        context.perform {
+            transactions.forEach { [weak self] transaction in
+                guard let self = self, let userInfo = transaction.objectIDNotification().userInfo else { return }
+                NSManagedObjectContext.mergeChanges(fromRemoteContextSave: userInfo, into: [self.context])
             }
         }
-        return (privateStoreFolderURL, sharedStoreFolderURL)
     }
 }
